7fb5c00b6a1c55d2994293ad4d5d85fef56da1ae
Vincent Samy <vincent.samy@lirmm.fr>
2016-10-25 17:43:40 +0200

    Better __init__.py file

--- binding/python/__init__.py
+++ binding/python/__init__.py
@@ -1,1 +1,13 @@
-from _mpcontroller import *
+from _mpcontroller import SolverFlag
+from _mpcontroller import PreviewSystem
+from _mpcontroller import TrajectoryConstrain
+from _mpcontroller import ControlConstrain
+from _mpcontroller import MPCTypeFull
+from _mpcontroller import MPCTypeLast
+
+__all__ = ["SolverFlag",
+           "PreviewSystem",
+           "TrajectoryConstrain",
+           "ControlConstrain",
+           "MPCTypeFull",
+           "MPCTypeLast"]
