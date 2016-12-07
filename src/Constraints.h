4804b72a1fe4525272224a2cdc4bcd430868d4cc
Vincent Samy <vincent.samy@lirmm.fr>
2016-12-07 10:41:39 +0100

    Add doxygen documentation

--- src/Constraints.h
+++ src/Constraints.h
@@ -25,20 +25,24 @@ namespace mpc {
 //forward declaration
 struct PreviewSystem;
 
+/**
+ * Flags to identify the type of the constraints
+ */
 enum class ConstraintFlag {
-    EqualityConstraint,
-    InequalityConstraint,
-    BoundConstraint
+    EqualityConstraint, /**< Equality constraint flag */
+    InequalityConstraint, /**< Inequality constraint flag */
+    BoundConstraint /**< Bound constraint tag */
 };
 
 /**
- * Constraint to add to the system.
- * This is a pure virtual class
+ * Abstract base class that represents Constraints.
+ * Any derived class of this one can be added to the PreviewController @see MPCTypeFull::addConstraint.
  */
 class Constraint {
 public:
     /**
      * Constructor of a constraint.
+     * @param name Name of the constraint
      */
     Constraint(const std::string& name);
 
@@ -66,8 +70,8 @@ public:
     virtual ConstraintFlag constraintType() = 0;
 
     /**
-     * Function that return the number of constrains.
-     * @return The number of constrains.
+     * Function that return the name of the constraint.
+     * @return The name of the constraint.
      */
     const std::string& name() const noexcept
     {
@@ -75,8 +79,8 @@ public:
     }
 
     /**
-     * Function that return the number of constrains.
-     * @return The number of constrains.
+     * Function that return the number of constraints.
+     * @return The number of constraints.
      */
     int nrConstr() noexcept
     {
@@ -88,15 +92,33 @@ protected:
     int nrConstr_;
 };
 
+/**
+ * Abstract Class for Equality and Inequality constraints.
+ * Even if Equality and Inequality constraints are different,
+ * their matrices are written the same way.
+ */
 class EqIneqConstraint : public Constraint {
 public:
-  EqIneqConstraint(const std::string &name, bool isInequalityConstraint);
+    /**
+     * Constructor of a constraint.
+     * @param name Name of the constraint
+     * @param isInequalityConstraint Whether the constraint is an Inequality (true) or an Equality (false).
+     */
+    EqIneqConstraint(const std::string& name, bool isInequalityConstraint);
 
-  const Eigen::MatrixXd &A()
-  {
-      return A_;
+    /**
+     * Get the 'A' matrix of the Equality/Inequality (\f$Ax\leq b\f$, \f$Ax = b\f$)
+     * @return The 'A' matrix of the constraint
+     */
+    const Eigen::MatrixXd& A()
+    {
+        return A_;
     }
 
+    /**
+     * Get the 'b' vector of the Equality/Inequality (\f$Ax\leq b\f$, \f$Ax = b\f$)
+     * @return The 'b' vector of the constraint
+     */
     const Eigen::VectorXd& b()
     {
         return b_;
@@ -108,11 +130,44 @@ protected:
     bool isIneq_;
 };
 
+/**
+ * Trajectory constraint.
+ * Depending on the parameter 'isInequalityConstraint' during the construction
+ * it can be an Equality constraints \f$Ex = f\f$
+ * or an Inequality constraints \f$Ex\leq f\f$.
+ */
 class TrajectoryConstraint final : public EqIneqConstraint {
 public:
+    /**
+     * Constructor of the trajectory constraint.
+     * Create a constraint of type \f$Ex\leq f\f$ or \f$Ex = f\f$.
+     * As \f$U\f$ is the optimization variable, \f$Ex\leq f\f$ or \f$Ex = f\f$
+     * is transformed to be \f$AU\leq b\f$ or \f$AU = b\f$.
+     * @warning \f$E\f$ and \f$f\f$ size must be the size of one iteration.
+     * @param E The matrix side of the constraint
+     * @param f The vector side of the constraint
+     * @param isInequalityConstraint Whether the constraint is an Inequality (true) or an Equality (false).
+     */
     TrajectoryConstraint(const Eigen::MatrixXd& E, const Eigen::VectorXd& f, bool isInequalityConstraint = true);
+
+    /**
+     * Initialize the constraint by resizing its inner matrices and vectors
+     * and setting the number of constraints.
+     * @param ps A preview system.
+     */
     void initializeConstraint(const PreviewSystem& ps) override;
+
+    /**
+     * Compute \f$A\f$ and \f$b\f$ from \f$E\f$, \f$f\f$ and the preview system.
+     * @param ps A preview system.
+     */
     void update(const PreviewSystem& ps) override;
+
+    /**
+     * Get the type of the constraint
+     * @return @see ConstraintFlag::InequalityConstraint if constructor's isInequalityConstraint is true
+     * @return @see ConstraintFlag::EqualityConstraint if constructor's isInequalityConstraint is false
+     */
     ConstraintFlag constraintType() override;
 
 private:
@@ -120,11 +175,45 @@ private:
     Eigen::VectorXd f_;
 };
 
+/**
+ * Control constraint.
+ * Depending on the parameter 'isInequalityConstraint' during the construction
+ * it can be an Equality constraints \f$Eu = f\f$
+ * or an Inequality constraints \f$Eu\leq f\f$.
+ * @todo Gives the possiblity to set directly \f$A\f$ and \f$b\f$. 
+ */
 class ControlConstraint final : public EqIneqConstraint {
 public:
+    /**
+     * Constructor of the control constraint.
+     * Create a constraint of type \f$Eu\leq f\f$ or \f$Eu = f\f$.
+     * As \f$U\f$ is the optimization variable, \f$Eu\leq f\f$ or \f$Eu = f\f$
+     * is transformed to be \f$AU\leq b\f$ or \f$AU = b\f$.
+     * @warning \f$E\f$ and \f$f\f$ size must be the size of one iteration.
+     * @param E The matrix side of the constraint
+     * @param f The vector side of the constraint
+     * @param isInequalityConstraint Whether the constraint is an Inequality (true) or an Equality (false).
+     */
     ControlConstraint(const Eigen::MatrixXd& E, const Eigen::VectorXd& f, bool isInequalityConstraint = true);
+
+    /**
+     * Initialize the constraint by resizing its inner matrices and vectors
+     * and setting the number of constraints.
+     * @param ps A preview system.
+     */
     void initializeConstraint(const PreviewSystem& ps) override;
+
+    /**
+     * Compute \f$A\f$ and \f$b\f$ from \f$E\f$, \f$f\f$ and the preview system.
+     * @param ps A preview system.
+     */
     void update(const PreviewSystem& ps) override;
+
+    /**
+     * Get the type of the constraint
+     * @return @see ConstraintFlag::InequalityConstraint if constructor's isInequalityConstraint is true
+     * @return @see ConstraintFlag::EqualityConstraint if constructor's isInequalityConstraint is false
+     */
     ConstraintFlag constraintType() override;
 
 private:
@@ -133,22 +222,83 @@ private:
     bool isIneq_;
 };
 
+/**
+ * Trajectory Bound constraint.
+ * Even if it is a bound constraint, the optimization vector is \f$U\f$ 
+ * so this constraint has to be transformed to an Inequality constraint.
+ * @warning This constraint is defined in the QP as an Inequality constraint. 
+ */
 class TrajectoryBoundConstraint final : public EqIneqConstraint {
 public:
+    /**
+     * Constructor of the trajectory Bound constraint.
+     * Create a constraint of type \f$\underbar{x}\leq x\leq\bar{x}\f$.
+     * As \f$U\f$ is the optimization variable,
+     * it is transformed to be \f$AU\leq b\f$.
+     * @warning \f$\underbar{x}\f$ and \f$\bar{x}\f$ size must be the size of one iteration.
+     * @param lower The lower bound \f$\underbar{x}\f$ of the constraint
+     * @param upper The upper bound \f$\bar{x}\f$ of the constraint
+     */
     TrajectoryBoundConstraint(const Eigen::VectorXd& lower, const Eigen::VectorXd& upper);
+
+    /**
+     * Initialize the constraint by resizing its inner matrices and vectors
+     * and setting the number of constraints.
+     * @param ps A preview system.
+     */
     void initializeConstraint(const PreviewSystem& ps) override;
+
+    /**
+     * Compute \f$A\f$ and \f$b\f$ from \f$E\f$, \f$f\f$ and the preview system.
+     * @param ps A preview system.
+     */
     void update(const PreviewSystem& ps) override;
+
+    /**
+     * Get the type of the constraint
+     * @return @see ConstraintFlag::InequalityConstraint
+     */
     ConstraintFlag constraintType() override;
 
 private:
     Eigen::VectorXd lower_, upper_;
 };
 
+/**
+ * Control Bound constraint.
+ * It bounds the optimization \f$\underbar{u}\leq u \leq\bar{u}\f$
+ * @todo Gives the possiblity to set directly \f$\underbar{U}\f$ and \f$\bar{U}\f$. 
+ */
 class ControlBoundConstraint final : public Constraint {
 public:
+    /**
+     * Constructor of the trajectory Bound constraint.
+     * Create a constraint of type \f$\underbar{u}\leq u\leq\bar{u}\f$.
+     * As \f$U\f$ is the optimization variable,
+     * it is transformed to be \f$\underbar{U}\leq U\leq\bar{U}\f$.
+     * @warning \f$\underbar{u}\f$ and \f$\bar{u}\f$ size must be the size of one iteration.
+     * @param lower The lower bound \f$\underbar{u}\f$ of the constraint
+     * @param upper The upper bound \f$\bar{u}\f$ of the constraint
+     */
     ControlBoundConstraint(const Eigen::VectorXd& lower, const Eigen::VectorXd& upper);
+
+    /**
+     * Initialize the constraint by resizing its inner matrices and vectors
+     * and setting the number of constraints.
+     * @param ps A preview system.
+     */
     void initializeConstraint(const PreviewSystem& ps) override;
+
+    /**
+     * Compute \f$\underbar{U}\f$ and \f$\bar{U}\f$ from \f$\underbar{u}\f$, \f$\bar{u}\f$ and the preview system.
+     * @param ps A preview system.
+     */
     void update(const PreviewSystem& ps) override;
+
+    /**
+     * Get the type of the constraint
+     * @return @see ConstraintFlag::BoundConstraint
+     */
     ConstraintFlag constraintType() override;
 
     const Eigen::VectorXd& lower()
