b54f00369c34468a042fc9a86d9d42a76f9ff57d
Vincent Samy <vincent.samy@lirmm.fr>
2017-01-19 15:47:25 +0100

    Organize include files and comments them.

--- src/PreviewSystem.cpp
+++ src/PreviewSystem.cpp
@@ -1,20 +1,21 @@
-// This file is part of ModelPreviewController.
+// This file is part of mpc.
 
-// ModelPreviewController is free software: you can redistribute it and/or
+// mpc is free software: you can redistribute it and/or
 // modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// ModelPreviewController is distributed in the hope that it will be useful,
+// mpc is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with ModelPreviewController.  If not, see
+// along with mpc.  If not, see
 // <http://www.gnu.org/licenses/>.
 
+// header
 #include "PreviewSystem.h"
 
 namespace mpc {
