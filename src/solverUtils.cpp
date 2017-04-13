f26987a4ea7eadad7b0e5265b8f82f4ee82f2166
Vincent Samy <vincent.samy@lirmm.fr>
2017-04-13 12:25:55 +0200

    correct unqieu_ptr initialization

--- src/solverUtils.cpp
+++ src/solverUtils.cpp
@@ -24,21 +24,21 @@ std::unique_ptr<SolverInterface> MPC_DLLAPI solverFactory(SolverFlag flag)
     switch (flag) {
 #ifdef EIGEN_LSSOL_FOUND
     case SolverFlag::LSSOL:
-        return std::unique_ptr<LSSOLSolver>();
+        return std::unique_ptr<LSSOLSolver>(new LSSOLSolver());
 #endif
 #ifdef EIGEN_GUROBI_FOUND
     case SolverFlag::GUROBIDense:
-        return std::unique_ptr<GUROBISolver>();
+        return std::unique_ptr<GUROBISolver>(new GUROBISolver());
 #endif
 #ifdef EIGEN_QLD_FOUND
     case SolverFlag::QLD:
-        return std::unique_ptr<QLDSolver>();
+        return std::unique_ptr<QLDSolver>(new QLDSolver());
 #endif
     // case SolverFlag::QuadProgSparse:
     //    return std::make_unique<QuadProgSparseSolver>();
     case SolverFlag::QuadProgDense:
     default:
-        return std::unique_ptr<QuadProgDenseSolver>();
+        return std::unique_ptr<QuadProgDenseSolver>(new QuadProgDenseSolver());
     }
 }
 
