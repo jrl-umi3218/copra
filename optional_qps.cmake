33ee488763d21f1c80bcf7e20bc53fb8601e34b7
Vincent Samy <vincent.samy@aist.go.jp>
2019-09-09 17:53:51 +0900

    First Updates. Not working in this state.

---
+++ optional_qps.cmake
@@ -0,0 +1,22 @@
+set(OPTIONAL_EIGEN_QPS
+    eigen-qld
+    eigen-lssol
+    eigen-gurobi
+)
+
+macro(add_optional_qps PACKAGE_LIST)
+    foreach(qp ${OPTIONAL_EIGEN_QPS})
+        find_package(${qp} QUIET)
+        if(${${qp}_FOUND})
+            list(APPEND PACKAGE_LIST ${qp})
+        endif()
+    endforeach()
+endmacro()
+
+macro(link_optional_qps project)
+    foreach(qp ${OPTIONAL_EIGEN_QPS})
+        if (${${qp}_FOUND})
+            target_link_libraries(${project} PUBLIC ${qp})
+        endif()
+    endforeach()
+endmacro()
